f877befcf7bc009adda899b4d2e64d44659e04d3
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-05-31 13:54:52 -0400

    Remove old model file.

--- predict/model.py
+++
@@ -1,9 +0,0 @@
-from generation import Generation
-from population import Population
-from node import Node
-
-
-def generate_population(size):
-    initial_generation = Generation(Node() for _ in range(size))
-    return Population(initial_generation)
-
