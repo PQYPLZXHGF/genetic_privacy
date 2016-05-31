5dd559ef2f76d0d7d8c89da7b00659826adb08b2
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-05-31 14:41:58 -0400

    Remove old kinship file.

--- predict/kinship.py
+++
@@ -1,6 +0,0 @@
-def kinship_coefficient(genome_1, genome_2, alleles, allele_frequency):
-    """
-    Calculate the kinship coefficient between two genomes as defined
-    in the paper
-    http://bioinformatics.oxfordjournals.org/content/26/22/2867.short
-    """
