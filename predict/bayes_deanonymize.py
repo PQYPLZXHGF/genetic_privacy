d456d5d3d04b6ff56f6305e9f7ee4613863c4180
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-09-24 16:27:16 -0400

    Start working on more vectorized code.

--- predict/bayes_deanonymize.py
+++ predict/bayes_deanonymize.py
@@ -60,25 +60,45 @@ class BayesDeanonymize:
         
     def identify(self, genome, actual_node):
         node_probabilities = dict() # Probability that a node is a match
-        shared_genome_cache = dict()
-        for member in self._population.members:
-            if member.genome is None:
-                continue
-            probabilities = self._compare_genome_node(member, genome,
+        shared = dict()
+        id_map = self._population.id_mapping
+        for labeled_node_id in length_classifier._labeled_nodes:
+            labeled_node = id_map[labeled_node_id]
+            s = shared_segment_length_genomes(genome, labeled_node.genome, 0)
+            shared[labeled_node] = s
+
+        node_data_i = dict()
+        batch_node_id = []
+        batch_labeled_node_id = []
+        batch_lengths = []
+        nodes = (member for member in self._population.members
+                 if member.genome is not None)
+        # for node, labeled_node in zip(nodes, length_classifier._labeled_nodes):
+        for node in nodes:
+            node_probs = []
+            node_start_i = len(batch_node_id)
+            for labeled_node_id in length_classifier._labeled_nodes:
+                labeled_node = id_map[labeled_node_id]
+                shared = shared[labeled_node]
+                if (node._id, labeled_node_id) not in length_classifier:
+                    if shared == 0:
+                        node_probs.append(INF_REPLACE)
+                    else:
+                        node_probs.append(ZERO_REPLACE)
+                else:
+                    batch_node_id.append(node._id)
+                    batch_labeled_node_id.append(labeled_node_id)
+                    batch_lengths.append(shared)
+
+        calc_prob = length_classifier.get_batch_probability(batch_lengths,
+                                                            batch_node_id,
+                                                            batch_labeled_node_id)
+        inf_or_nan = np.logical_or(np.isnan(calc_prob), np.isinf(calc_prob))
+        calc_prob[inf_or_nan] = INF_REPLACE
+        calc_prob[calc_prob == 0.0] = ZERO_REPLACE
+                    probabilities = self._compare_genome_node(member, genome,
                                                       shared_genome_cache)
             node_probabilities[member] = np.sum(np.log(probabilities))
-        # rank = sorted(list(node_probabilities.items()),
-        #               key = lambda x: x[1])
-        # potential_node = rank[-1][0]
-        # import pdb
-        # pdb.set_trace()
-        # potential_node_probs = self._compare_genome_node(potential_node,
-        #                                                  genome,
-        #                                                  shared_genome_cache)
-        # actual_node_probs = self._compare_genome_node(actual_node,
-        #                                               genome,
-        #                                               shared_genome_cache)
-
         potential_node = max(node_probabilities.items(),
                              key = lambda x: x[1])[0]
         return get_sibling_group(potential_node)
