1822c55e13cdc7e254eb4fe6031c86ff221f8568
Paul Ellenbogen <pe5@cs.princeton.edu>
2016-10-10 13:54:56 -0400

    Add option to pass certain nodes to identify.

--- predict/evaluate_deanonymize.py
+++ predict/evaluate_deanonymize.py
@@ -13,6 +13,7 @@ parser = ArgumentParser(description = "Evaluate performance of classification.")
 parser.add_argument("population")
 parser.add_argument("classifier")
 parser.add_argument("--num_node", "-n", type = int, default = 10)
+parser.add_argument("--test_node", "-t", type = int, action = "append")
 args = parser.parse_args()
 
 
@@ -34,16 +35,18 @@ bayes = BayesDeanonymize(population, classifier)
 id_mapping = population.id_mapping
 labeled_nodes = set(id_mapping[node_id] for node_id
                     in classifier._labeled_nodes)
-
-seed(98104)
-unlabeled = sample(list(nodes - labeled_nodes),
-                   args.num_node)
+if args.test_node is not None and len(args.test_node) > 0:
+    unlabeled = [id_mapping[node_id] for node_id in args.test_node]
+else:
+    unlabeled = sample(list(nodes - labeled_nodes),
+                       args.num_node)
 # unlabeled = [choice(list(set(last_generation) - labeled_nodes))]
 # correct_nodes = [id_mapping[x] for x in [93437, 92902]]
 # incorrect_nodes = [id_mapping[x] for x in [92635, 94587]]
 # unlabeled = sample(incorrect_nodes, args.num_node)
 correct = 0
 incorrect = 0
+incorrect_examples = set()
 print("Attempting to identify {} random nodes.".format(len(unlabeled)))
 for i, node in enumerate(unlabeled):
     print("Iteration: {}, actual node ID: {}".format(i + 1, node._id))
@@ -53,6 +56,7 @@ for i, node in enumerate(unlabeled):
         correct += 1
         print("correct")
     else:
+        incorrect_examples.add(node._id)
         print("incorrect")
         incorrect += 1
 
@@ -60,3 +64,4 @@ for i, node in enumerate(unlabeled):
 print("{} correct, {} incorrect, {} total.".format(correct, incorrect,
                                                   len(unlabeled)))
 print("{} percent accurate.".format(correct / len(unlabeled)))
+print("Incorrectly guessed nodes: {}".format(incorrect_examples))
