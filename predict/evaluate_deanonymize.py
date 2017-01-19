b407178347cd97111ca08f45a3c7df572cad7328
Paul Ellenbogen <pe5@cs.princeton.edu>
2017-01-19 16:06:59 -0500

    More output, change path error output.

--- predict/evaluate_deanonymize.py
+++ predict/evaluate_deanonymize.py
@@ -66,6 +66,9 @@ incorrect_examples = set()
 incorrect_distances = []
 from_error = []
 to_error = []
+one_path_error = 0
+both_path_error = 0
+no_path_error = 0
 no_common_ancestor = 0
 print("Attempting to identify {} random nodes.".format(len(unlabeled)))
 for i, node in enumerate(unlabeled):
@@ -84,9 +87,19 @@ for i, node in enumerate(unlabeled):
             error = error_between_nodes(node, labeled_node,
                                         population.node_to_generation,
                                         False)
-            if len(error[0]) > 0 or len(error[1]) > 0:
-                from_error.append(len(error[0]))
-                to_error.append(len(error[1]))
+            path_error = 0
+            if len(error[0]) > 0:
+                path_error += 1
+                from_error.append(error[0][0])
+            if len(error[1]) > 0:
+                path_error += 1
+                to_error.append(error[1][0])
+            if path_error == 1:
+                one_path_error += 1
+            if path_error == 2:
+                both_path_error += 1
+            else:
+                no_path_error += 1
         rca, distance = recent_common_ancestor(node, next(iter(identified)),
                                                population.node_to_generation)
         if distance is None:
@@ -103,4 +116,6 @@ print("Relationship distance stats: {}".format(stats.describe(incorrect_distance
 print("No common ancestor occured {} times.".format(no_common_ancestor))
 print("Error from correct node to rca stats: {}".format(stats.describe(from_error)))
 print("Error from labeled node to rca stats: {}".format(stats.describe(to_error)))
+total_path_error = one_path_error + both_path_error + no_path_error
+print("Fraction there is error on one side of path: {}, both sides: {}".format(one_path_error / total_path_error, both_path_error / total_path_error))
 
